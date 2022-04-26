fc68d8f5b8d54157a0661b54b14e22c1facf841c
crow <59628672+xcrowpk@users.noreply.github.com>
2022-04-25 21:36:33 -0300

    Delete mvt_app directory

--- mvt_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from  .models import * #importamos el archivo models
-
-# Register your models here.
-admin.site.register(Services)
-admin.site.register(Clients)
-admin.site.register(Staff)
